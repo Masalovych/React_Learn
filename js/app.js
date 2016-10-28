616b7f86f7ad8642d43ea0f6a09fa529f0307e0f
Dmytro Masalovych <dmasalovich@rocketfuel.com>
2016-10-28 15:19:08 +0300

    First day learning ))

--- js/app.js
+++ js/app.js
@@ -1,44 +1,68 @@
-var my_news = [
-  {
-    author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+var my_news = [{
+  author: 'Саша Печкин',
+  text: 'В четчерг, четвертого числа...',
+  bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+}, {
+  author: 'Просто Вася',
+  text: 'Считаю, что $ должен стоить 35 рублей!',
+  bigText: 'А евро 42!'
+}, {
+  author: 'Гость',
+  text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+  bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+}];
+var Article = React.createClass({
+  propTypes: {
+    data: React.PropTypes.shape({
+      author: React.PropTypes.string.isRequired,
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
+    })
   },
-  {
-    author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+  getInitialState: function() {
+    return {
+      visible: false
+    };
   },
-  {
-    author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  readmoreClick: function(e) {
+    e.preventDefault();
+    this.setState({visible: true});
+  },
+  render: function() {
+    var author = this.props.data.author,
+      text = this.props.data.text,
+      bigText = this.props.data.bigText,
+      visible = this.state.visible;
+    return (
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__text'>{text}</p>
+        <a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none': '')}>Подробнее</a>
+        <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+      </div>
+    )
   }
-];
-
+});
 var News = React.createClass({
   render: function() {
     var data = this.props.data;
-    var newsTemplate = data.map(function(item, index) {
-      return (
-        <div key={index}>
-          <p className="news__author">{item.author}:</p>
-          <p className="news__text">{item.text}</p>
-        </div>
-      )
-    });
-
+    var newsTemplate;
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key={index}>
+            <Article data={item} />
+          </div>
+        )
+      })
+    } else {
+      newsTemplate = <p>К сожалению новостей нет</p>
+    }
     return (
-      <div className="news">
+      <div className='news'>
         {newsTemplate}
-      </div>
-    );
-  }
-});
-
-
-var Comments = React.createClass({
-  render: function() {
-    return (
-      <div className="comments">
-        Нет новостей - комментировать нечего.
+        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего ново
+          стей: {data.length}</strong>
       </div>
     );
   }
@@ -46,14 +70,14 @@ var Comments = React.createClass({
 var App = React.createClass({
   render: function() {
     return (
-      <div className="app">
-        Всем привет, я компонент App! Я умею отображать новости.
-        <News data = {my_news}/>
-        <Comments />
+      <div className='app'>
+        <h3>Новости</h3>
+        <News data={my_news}/>
       </div>
     );
   }
 });
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
